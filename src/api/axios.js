c35a16069142aa414f873858c51598950e6497af
Danujainoshika <danujamanukularathna@gmail.com>
2026-04-20 13:07:08 +0530

    update baseURL in axios configuration to production server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "http://13.232.231.29:5000/api",
     headers: {
         "Content-Type": "application/json",
     },
     timeout: 10000,
 })
 
-export default api;
